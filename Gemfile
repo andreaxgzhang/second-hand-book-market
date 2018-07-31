4176f2de1d253ce9aac8ebd3318144192b130a94
Xiange Zhang <xz4ee@virginia.edu>
2018-07-31 19:33:13 +0800

    photo

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'uglifier'
 gem 'webpacker'
 gem 'faker'
 gem 'rest-client'
+gem 'pg_search'
 group :development do
   gem 'web-console', '>= 3.3.0'
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,16 @@
 class Post < ApplicationRecord
+  include PgSearch
+
   # has_one :transaction
-  belongs_to :user
-  validates :title, presence: true
-  validates :price, presence: true, numericality: { only_integer: true }
-  validates :course_number, presence: true
-  validates :professor, presence: true
+  # belongs_to :user
+  # validates :title, presence: true
+  # validates :price, presence: true, numericality: { only_integer: true }
+  # validates :course_number, presence: true
+  # validates :professor, presence: true
+  pg_search_scope :global_search,
+                  against: [ :title, :description, :course_number, :professor ],
+                  using: {
+                          tsearch: { prefix: true } # <-- now `superman batm` will return something!
+                          }
+
 end
