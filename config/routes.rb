07deb11a22cbd18ad8ed80924da765aa5bbb7f4a
Xiange Zhang <xz4ee@virginia.edu>
2018-08-03 13:26:43 +0800

    add transation index routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,14 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :posts do
-        resources :transactions, only:[:index, :create, :update]
+        resources :transactions, only:[:create, :update]
       end
       resources :users, only:[:index, :show, :update] do
         member do
           get '/purchased', to: 'users#purchased'
         end
       end
+      get '/transactions', to: 'transactions#index'
       get '/sold', to: 'users#sold'
       post '/login', to: 'login#login'
     end
