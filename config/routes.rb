e818f4166029dfebf014df703c4eeb27894b2385
Xiange Zhang <xz4ee@virginia.edu>
2018-08-03 12:52:57 +0800

    transaction routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   # [...]
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :posts
+      resources :posts do
+        resources :transactions, only:[:index, :create, :update]
+      end
       resources :users, only:[:index, :show, :update] do
         member do
           get '/purchased', to: 'users#purchased'
         end
       end
-      resources :transactions, only:[:index, :create, :update]
       get '/sold', to: 'users#sold'
       post '/login', to: 'login#login'
     end
